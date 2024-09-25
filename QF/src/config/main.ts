8430cd12ecc69923300e22d05cb8211acd467fbd
Nikita <fedosovnikita007@gmail.com>
2024-09-25 20:08:00 +0300

    Коммит для прода

--- QF/src/config/main.ts
+++ QF/src/config/main.ts
@@ -1,6 +1,8 @@
-const BASE_URL = 'http://localhost:8003'; //'https://qb.paia1nik.ru'
+const BASE_URL = 'https://qb.paia1nik.ru'; //'http://localhost:8003';
 
 const REQ_ENDPOINT = BASE_URL + '/api/post/'
 const VPN_ENDPOINT = BASE_URL + '/api/qvpn/'
 
 export { BASE_URL, REQ_ENDPOINT, VPN_ENDPOINT }
+
+// двести семдясят миллиардов триста петясят девять миллионов семьдясят тысяч один

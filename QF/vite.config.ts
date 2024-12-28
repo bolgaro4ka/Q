8e6b59c0adf073189058f9b7f6ffe0c7f9e5b3f4
Nikita <bolgaro4ka.github@gmail.com>
2024-12-28 14:44:09 +0300

    Update vite.config.ts

--- QF/vite.config.ts
+++ QF/vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   server: {
     port: 4002, // change to your desired port
     host: '0.0.0.0',
